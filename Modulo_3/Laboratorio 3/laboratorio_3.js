2ffe4493bbee12a38608f17fbf67eec5cfcce67e
jonathan calderon <jonathancalde2010@gmail.com>
2024-10-24 20:21:08 -0500

    Create laboratorio_3.js
    cree el js

---
+++ Modulo_3/Laboratorio 3/laboratorio_3.js
@@ -0,0 +1,5 @@
+//Aquí desarrollaremos el laboratorio tres
+//en la medida de lo posible sería bueno dejar comentado cada ejercicio para que sea más fácil 
+//identificar cada punto al momento de arreglar el PDF
+//según lo acordado Brayan hace el primer punto, aleja hace el segundo y Jonathan el tercer punto
+//El cuarto punto está por definir para hacer un punto cada uno del ejercicio 4.
