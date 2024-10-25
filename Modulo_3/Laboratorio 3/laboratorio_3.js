c58f6be740ddd0a92a47ab76cf91ce8753d6a234
jonathan calderon <jonathancalde2010@gmail.com>
2024-10-24 21:53:02 -0500

    Update laboratorio_3.js
    ejercicio 3 resuelto

--- Modulo_3/Laboratorio 3/laboratorio_3.js
+++ Modulo_3/Laboratorio 3/laboratorio_3.js
@@ -3,3 +3,36 @@
 //identificar cada punto al momento de arreglar el PDF
 //según lo acordado Brayan hace el primer punto, aleja hace el segundo y Jonathan el tercer punto
 //El cuarto punto está por definir para hacer un punto cada uno del ejercicio 4.
+
+//Desarrollo ejercicio 3
+
+//Una función tradicional que salude al usuario
+
+function saludar (nombre) {
+    console.log(`Hola ${nombre}`)
+}
+saludar("Jonathan")
+
+//Una función flecha que calcule el total con impuesto
+
+let total = (valor, impuesto)  => {
+        let total
+        impuesto = impuesto / 100
+        total = valor + (valor * impuesto)
+        console.log(`El valor total a pagar ya con el impuesto de iva incluido es $${total}`)
+}
+
+total(1000000,19)
+
+//Una función que reciba un callback simple
+
+const PAISES = ['Colombia', 'Argentina', 'Brasil']
+
+function agregarPais (lista, callback){
+    lista.push ('Perú')
+    callback(lista)
+}
+
+agregarPais(PAISES, function(lista) {
+    console.log(`He agregado un país al array y ahora es de ${lista.length} elementos`)
+})
