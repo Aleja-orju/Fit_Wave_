78594c60189441d695579511cbf8eb5d6f1a9f47
Aleja-orju <alejandraorto08@gmail.com>
2024-10-31 19:03:10 -0500

    punto 4

--- Modulo_3/Laboratorio 3/laboratorio_3.js
+++ Modulo_3/Laboratorio 3/laboratorio_3.js
@@ -33,12 +33,12 @@ if (precio1>precio2){
     console.log("El valor menor es el precio 1: ",precio1);
 }
 
+//Bucle del 1 al 5
 for (let i = 1; i <= 5; i++) {
     console.log(i)
 }
 
 //Una función tradicional que salude al usuario
-
 function saludar (nombre) {
     console.log(`Hola ${nombre}`)
 }
@@ -56,7 +56,6 @@ let total = (valor, impuesto)  => {
 total(1000000,19)
 
 //Una función que reciba un callback simple
-
 const PAISES = ['Colombia', 'Argentina', 'Brasil']
 
 function agregarPais (lista, callback){
@@ -67,3 +66,29 @@ function agregarPais (lista, callback){
 agregarPais(PAISES, function(lista) {
     console.log(`He agregado un país al array y ahora es de ${lista.length} elementos`)
 })
+
+// Objeto para convertir objeto a string JSON
+const usuario = { 
+    nombre: "Alejandra",
+    edad: 19
+};
+
+// Conversi n a JSON
+const usuarioJSON = JSON.stringify(usuario);
+console.log("El objeto convertido a JSON: ", usuarioJSON)
+
+// Promesa simple
+const miPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Promesa resuelta después de 2 segundos");
+    }, 2000);
+});
+
+// Usar la promesa
+miPromesa
+    .then((resultado) => {
+        console.log(resultado);
+    })
+    .catch((error) => {
+        console.error("Error en la promesa:", error);
+    });
